fb26abc487ccec3056cc189f2d9a2711bd2184cd
ChocolateAceCream <nuodi@hotmail.com>
2018-02-06 21:03:44 -0500

    add button and icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MaterialModule} from './material.module';
@@ -11,7 +11,7 @@ import { MaterialModule} from './material.module';
   ],
   imports: [
       BrowserModule,
-      BrowserAnimationModule,
+      BrowserAnimationsModule,
       MaterialModule
   ],
   providers: [],
