83b75624459be7359e691995d5af974efeb525e4
ChocolateAceCream <ubuntu@ip-172-31-62-146.ec2.internal>
2018-02-20 15:26:06 -0500

    finished login page, next work on nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -32,7 +32,9 @@ import { AppRoutingModule } from './app-routing.module';
       MaterialModule,
 	  FlexLayoutModule,
       AppRoutingModule,
-      FormsModule
+      FormsModule,
+      ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
